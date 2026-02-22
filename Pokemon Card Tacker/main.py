33af74fcd86aaa3a7927bf865c145578f0a12039
Aaron Thung <44ront@gmail.com>
2026-02-21 16:01:51 -0800

    Update main.py

--- Pokemon Card Tacker/main.py
+++ Pokemon Card Tacker/main.py
@@ -9,10 +9,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG", "true").lower() == "true")
 
-
-"""
-//cd "C:\Users\Aaron\idk\Pokemon-Card-Tracker\Pokemon Card Tacker"
-.venv\Scripts\python.exe main.py
-
-http://127.0.0.1:5000
-"""
